5364db61536cbe111ff04798de2bb98fbfc72203
eduar <eduar@EDUARDO>
2025-09-07 04:36:59 -0400

    update

---
+++ article_001.js
@@ -0,0 +1,97 @@
+const PageArticle001_ArticleTitle001 = document.getElementById("PageArticle001_ArticleTitle001");
+const ArticleTitle001_article_title_heading = document.getElementById("ArticleTitle001_article_title_heading");
+const PageArticle001_ArticleBody001 = document.getElementById("PageArticle001_ArticleBody001");
+const ArticleTitle001_reload_article_button = document.getElementById("ArticleTitle001_reload_article_button");
+
+const PageArticle001 = document.getElementById("PageArticle001");
+const webUrl = `static/json/${PageArticle001.dataset.contentFilename}.json`;
+
+setTimeout(()=>{
+    reloadArticle(webUrl)
+},100)
+
+document.addEventListener('keydown', (event) => {
+    if (event.key.toLowerCase() === 'c') {
+        reloadArticle(webUrl)
+    }
+});
+
+let touchCount = 0;
+let timeoutId = null;
+document.addEventListener('pointerdown', (event) => {
+  event.preventDefault();
+  touchCount++;
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    touchCount = 0;
+    console.log('Touch counter reset.');
+  }, 300); 
+  if (touchCount === 3) {
+    reloadArticle(webUrl)
+    touchCount = 0;
+    clearTimeout(timeoutId);
+  }
+});
+
+ArticleTitle001_reload_article_button.addEventListener('click',()=>{
+    PageArticle001_ArticleBody001.innerHTML = "Reloading"
+    setTimeout(()=>{
+        reloadArticle(webUrl)
+    },100)
+})
+
+function reloadArticle(url){
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        ArticleTitle001_article_title_heading.innerHTML = data.title;
+        
+        let htmlContent = ""; 
+        data.ideas.forEach(idea => {
+            if(idea.heading)
+            {
+                htmlContent += `<h2>${idea.heading}</h2>`;
+            }
+            idea.subideas.forEach(subidea => {
+              if(subidea.type === "biblical_quote")
+              {
+                if(subidea.key_phrases){
+                  console.log(subidea.key_phrases)
+                  subidea.key_phrases.forEach(key_phrase => {
+                    
+                    console.log(key_phrase)
+                    subidea.content = subidea.content.replace(key_phrase, `<span class="biblical_quote_key_prhase">${key_phrase}</span>`)
+                  });
+                }
+                htmlContent += `
+                <div class="biblical_quote">
+                  <p class="biblical_quote_text">${subidea.content}</p>
+                  <p class="biblical_quote_reference">${subidea.biblical_reference}</p>
+                </div>
+                `
+              }
+              else if(subidea.type === "image"){
+                htmlContent += `
+                <div class="image">
+                  <img src="static/images/${subidea.content}">
+                </div>
+                `
+              }              
+              else
+              {
+                htmlContent += `<p>${subidea.content}</p>`;
+              }
+            });
+        });
+        PageArticle001_ArticleBody001.innerHTML = htmlContent;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        PageArticle001_ArticleBody001.innerHTML = `<p>Error loading content.</p>`; 
+    });
+}
