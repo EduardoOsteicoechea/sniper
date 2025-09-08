29dd42e7b26e8bc0289ea3fc66cb00645efd6a9c
eduar <eduar@EDUARDO>
2025-09-08 04:46:06 -0400

    update

--- vehicle_certificate_generator.js
+++ vehicle_certificate_generator.js
@@ -122,7 +122,7 @@ export default class VehicleCertificateGenerator {
     }
 
     StorePageDatasetElement() {
-        this.pageDatasetElement = document.getElementById(page_dataset_attributes)
+        this.pageDatasetElement = document.getElementById("page_dataset_attributes")
     }
 
     GenerateGeneratePdfButton() {
