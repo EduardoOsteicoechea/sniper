6ab774f3222daaeab9a3f2707300d4c303646f52
eduar <eduar@EDUARDO>
2025-09-08 07:40:51 -0400

    update

--- input_validation.js
+++ input_validation.js
@@ -1,5 +1,5 @@
 export default class InputValidationBase {
-    alphabeticCharacters = [" ",".", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "á", "é", "í", "ó", "ú", "ü", "Á", "É", "Í", "Ó", "Ú", "Ü"]
+    alphabeticCharacters = [" ", ".", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "á", "é", "í", "ó", "ú", "ü", "Á", "É", "Í", "Ó", "Ú", "Ü"]
     numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     numberOfCharacters = 0;
     inputName = "";
@@ -10,8 +10,19 @@ export default class InputValidationBase {
         inputName,
         validationType = 1
     ) {
-        this.numberOfCharacters = numberOfCharacters
-        this.inputName = inputName
+        if (Number.isNaN(numberOfCharacters)) {
+            throw new Error("numberOfCharacters must be a valid number.");
+        }
+        this.numberOfCharacters = numberOfCharacters;
+
+        if (inputName === "") {
+            throw new Error("Provide a name for the input.");
+        }
+        this.inputName = inputName;
+
+        if (validationType < 1 || validationType > 3) {
+            throw new Error("Invalid validation type. Please use 1, 2, or 3.");
+        }
         switch (validationType) {
             case 1:
                 this.allowedCharacters = new Set(this.alphabeticCharacters);
@@ -21,7 +32,7 @@ export default class InputValidationBase {
                 break;
             case 3:
                 this.allowedCharacters = new Set([...this.alphabeticCharacters, ...this.numericCharacters]);
-                break;        
+                break;
             default:
                 this.allowedCharacters = new Set([...this.alphabeticCharacters, ...this.numericCharacters]);
                 break;
@@ -34,12 +45,8 @@ export default class InputValidationBase {
 
     validation(input) {
         input.addEventListener("input", () => {
-            const inputValue = input.value;
 
-            console.log("input.value")
-            console.log(inputValue)
-            console.log("this.numberOfCharacters")
-            console.log(this.numberOfCharacters)
+            const inputValue = input.value;
 
             if (inputValue.length > this.numberOfCharacters) {
                 this.displayError(`"${this.numberOfCharacters}" es el máximo de caracteres para ${this.inputName}.`)
@@ -49,13 +56,15 @@ export default class InputValidationBase {
             }
 
             let sanitizedValue = '';
+
             for (const character of input.value) {
                 if (this.allowedCharacters.has(character)) {
                     sanitizedValue += character;
                 } else {
-                    this.displayError(`${character} es inválido para ${this.inputName}.`);
+                    this.displayError(`"${character}" es inválido para ${this.inputName}.`);
                 }
             }
+
             input.value = sanitizedValue;
         })
     }
