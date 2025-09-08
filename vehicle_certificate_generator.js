fe48ddf77f5e2b0b0ebea6763b0aa09c9a85db06
eduar <eduar@EDUARDO>
2025-09-08 10:45:59 -0400

    update

--- vehicle_certificate_generator.js
+++ vehicle_certificate_generator.js
@@ -65,16 +65,15 @@ export default class VehicleCertificateGenerator {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
-
                 return response.json();
             })
             .then(data => {
                 console.log("Success:", data);
-
                 return data;
             })
             .catch(error => {
                 console.error("Error:", error);
+                throw error;
             });
     }
     
