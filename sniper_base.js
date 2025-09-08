eedf20d1c30dc497ac55e93df37446b2de836e9e
eduar <eduar@EDUARDO>
2025-09-08 04:53:30 -0400

    update

--- sniper_base.js
+++ sniper_base.js
@@ -1,6 +1,7 @@
 import VehicleCertificateGenerator from "./vehicle_certificate_generator.js";
 
+
 const vehicleCertificateGenerator = new VehicleCertificateGenerator(
-    document.getElementsByTagName("main")[0], 
-    "https://eduardoos.com/"
+    document.getElementById("main")[0], 
+    "https://eduardoos.com"
 )
